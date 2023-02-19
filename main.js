356ccdcd79800f2703b403209ec7a316df824d46
bekirbali <bfbali43@gmail.com>
2023-02-19 12:32:15 +0300

    flexBox

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
+// const panels = document.querySelectorAll(".panel");
+
+// function toggleOpen() {
+//   console.log("Hello");
+//   this.classList.toggle("open");
+// }
+
+// function toggleActive(e) {
+//   console.log(e.propertyName);
+//   if (e.propertyName.includes("flex")) {
+//     this.classList.toggle("open-active");
+//   }
+// }
+
+// panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+// panels.forEach((panel) =>
+//   panel.addEventListener("transitionend", toggleActive)
+// );
+
 const panels = document.querySelectorAll(".panel");
 
-function toggleOpen() {
-  console.log("Hello");
-  this.classList.toggle("open");
-}
-
-function toggleActive(e) {
-  console.log(e.propertyName);
-  if (e.propertyName.includes("flex")) {
-    this.classList.toggle("open-active");
-  }
-}
-
-panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
 panels.forEach((panel) =>
-  panel.addEventListener("transitionend", toggleActive)
+  panel.addEventListener("click", (e) => {
+    e.currentTarget.classList.toggle("active");
+    e.currentTarget.classList.toggle("open");
+  })
 );
